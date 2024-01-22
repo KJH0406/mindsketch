9f33b6df77433d6c202243e08ce31ef58147a8e1
김장호 <rlawkdgh0406@naver.com>
2024-01-22 11:16:19 +0900

    docs: 환경 세팅

--- site.config.js
+++ site.config.js
@@ -1,25 +1,25 @@
 const CONFIG = {
   // profile setting (required)
   profile: {
-    name: "morethanmin",
+    name: "mindsketch",
     image: "/avatar.svg", // If you want to create your own notion avatar, check out https://notion-avatar.vercel.app
     role: "frontend developer",
-    bio: "I develop everything using node.",
-    email: "morethanmin.dev@gmail.com",
-    linkedin: "morethanmin",
-    github: "morethanmin",
+    bio: "I develop everything.",
+    email: "rlawkdgh0406@naver.com",
+    linkedin: "",
+    github: "KJH0406",
     instagram: "",
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `mindsketch`,
+      href: "https://github.com/KJH0406/mindsketch",
     },
   ],
   // blog setting (required)
   blog: {
-    title: "morethan-log",
-    description: "welcome to morethan-log!",
+    title: "mindsketch",
+    description: "welcome to mindsketch!",
   },
 
   // CONFIG configration (required)
